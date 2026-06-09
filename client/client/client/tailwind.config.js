ef5e7228fbc03861e58df65e0503801505cf1b9d
Fuadazman <fuadazman44@gmail.com>
2026-06-09 03:08:16 +0300

    Create tailwind.config.js

---
+++ client/client/client/tailwind.config.js
@@ -0,0 +1,19 @@
+
+```javascript
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#6F00FF",
+        bg: "#F9FAFB"
+      }
+    }
+  },
+  plugins: []
+}
+
+```
